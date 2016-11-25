bf04490e4006d731f4fb6b9106829045067ec01a
Swapnil Raj <raj.swapnil99@gmail.com>
2016-11-25 23:13:10 +0000

    Found broken module import.

--- OCRtest.py
+++ OCRtest.py
@@ -8,10 +8,10 @@ gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 # Now we split the image to 5000 cells, each 20x20 size
 cells = [np.hsplit(row,100) for row in np.vsplit(gray,50)]
 
- # Make it into a Numpy array. It size will be (50,100,20,20)
+# Make it into a Numpy array. It size will be (50,100,20,20)
 x = np.array(cells)
 
- # Now we prepare train_data and test_data.
+# Now we prepare train_data and test_data.
 train = x[:,:50].reshape(-1,400).astype(np.float32) # Size = (2500,400)
 test = x[:,50:100].reshape(-1,400).astype(np.float32) # Size = (2500,400)
 
@@ -21,9 +21,9 @@ train_labels = np.repeat(k,250)[:,np.newaxis]
 test_labels = train_labels.copy()
 
 # Initiate kNN, train the data, then test it with test data for k=1
-knn = cv2.ML.KNearest_create()
-knn.train(train, cv2.ml.ROW_SAMPLE, train_labels)
-ret,result,neighbours,dist = knn.findNearest(test,k=5)
+knn = cv2.KNearest()
+knn.train(train,train_labels)
+ret,result,neighbours,dist = knn.find_nearest(test,k=5)
 
 # Now we check the accuracy of classification
 # For that, compare the result with test_labels and check which are wrong
@@ -31,10 +31,11 @@ matches = result==test_labels
 correct = np.count_nonzero(matches)
 accuracy = correct*100.0/result.size
 print accuracy
- # save the data
+
+# save the data
 np.savez('knn_data.npz',train=train, train_labels=train_labels)
 
- # Now load the data
+# Now load the data
 with np.load('knn_data.npz') as data:
     print data.files
     train = data['train']
